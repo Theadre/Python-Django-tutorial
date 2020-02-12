1dae12828829338d24e0e40d8984a2885c9815ce
Jérémy <jeremy.taswinalkasid@gmail.com>
2020-02-12 18:36:20 +1300

    step5

---
+++ taswinalkasid_jeremy/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for taswinalkasid_jeremy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taswinalkasid_jeremy.settings')
+
+application = get_asgi_application()
